d35a5529371706e5a9fafd5f24b2ad10f31042ff
sivaprasadnk <sivaprasadnk123@gmail.com>
2024-07-07 21:23:36 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Use the PORT environment variable if it exists, otherwise use 3000
 const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send(`Hello! I'm a passionate Flutter developer with 3.5 years of experience crafting seamless, cross-platform applications that delight users and solve real-world problems. My journey in software development is driven by a relentless curiosity and a commitment to learning new technologies and methodologies.
@@ -8,6 +14,10 @@ app.get('/', (req, res) => {
   I'm always eager to explore new ideas and enhance my skill set, continuously pushing the boundaries of what's possible with Flutter. Let's connect and build something amazing together!`);
 });
 
+app.get('/contact', (req, res) => {
+  res.send(`If you'd like to get in touch, please reach out via email at [your-email@example.com] or connect with me on LinkedIn.`);
+});
+
 app.listen(port, () => {
-  console.log(`Server is running at here http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
