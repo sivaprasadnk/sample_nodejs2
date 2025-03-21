9db8d07ed4722a06d909ba324ba337e0cbd81da8
Sivaprasad NK <75713903+sivaprasadnk@users.noreply.github.com>
2025-03-21 14:17:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about-me', (req, res) => {
-  // var data = `Hello! I'm a passionate Flutter developer with 3.9 years of experience crafting seamless, cross-platform applications that delight users and solve real-world problems. My journey in software development is driven by a relentless curiosity and a commitment to learning new technologies and methodologies.
+  // var data = `Hello! I'm a passionate Flutter developer with 3.5+ years of experience crafting seamless, cross-platform applications that delight users and solve real-world problems. My journey in software development is driven by a relentless curiosity and a commitment to learning new technologies and methodologies.
   // Beyond the world of coding, I'm a fitness enthusiast dedicated to maintaining a balanced and healthy lifestyle. As a proud felintrovert, I cherish my moments of solitude with my feline companions, finding inspiration and creativity in these quiet times.
   // I'm always eager to explore new ideas and enhance my skill set, continuously pushing the boundaries of what's possible with Flutter. Let's connect and build something amazing together!`;
 
